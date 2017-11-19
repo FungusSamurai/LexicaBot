75ed06e264f07680ed9cd8289664dc476d4f7d44
FungusSamurai <brandon.tmacauley@gmail.com>
2017-11-19 18:34:09 -0500

    no message

--- TreasureBot/index.js
+++ TreasureBot/index.js
@@ -5,14 +5,17 @@ bot.on('message', (message) => {
 
     if (message.content == 'lexisleep')
     {
-        bot.user.message('DEBUG: Going to Sleep');
+        channel.message('DEBUG: Going to Sleep');
+        bot.user.setStatus({status: "invisible"});
         //bot.user.setPresence({ status: "invisible", game: { name: 'Dreams' }});
     }
 
     if (message.content == 'lexiwake')
     {
+        channel.message('DEBUG: Waking Up');
+        bot.user.setStatus({status: "online"});
        // bot.user.setPresence({ status: "online", game: { name: 'CraftMine' }});
-        bot.user.message('DEBUG Good Day.');
+       // bot.user.message('DEBUG Good Day.');
     }
 
     if (message.content == 'quoteth the raven')
