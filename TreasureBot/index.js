7561d88b705bb524f66e927347731141bbe5083c
FungusSamurai <brandon.tmacauley@gmail.com>
2017-11-19 17:54:36 -0500

    adsf

--- TreasureBot/index.js
+++ TreasureBot/index.js
@@ -3,14 +3,15 @@ const bot = new Discord.Client();
 
 bot.on('message', (message) => {
 
-    if (message.content == 'LexiSleep')
+    if (message.content == 'lexisleep')
     {
-        bot.user.setPresence('invisible', false);
+        bot.user.message('Going to Sleep');
+        bot.user.setPresence({ status: 'invisible', game: { name: 'Dreams' } });
     }
 
-    if (message.content == 'LexiWake')
+    if (message.content == 'lexiwake')
     {
-        bot.user.setPresence('online', false);
+        bot.user.setPresence({ status: 'online', game: { name: 'CraftMine' } });
         bot.user.message('Good Day.');
     }
 
