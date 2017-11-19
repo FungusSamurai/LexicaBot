17ebff8845272784fccfb3e71424ebab01ffc6e6
FungusSamurai <brandon.tmacauley@gmail.com>
2017-11-19 17:46:12 -0500

    set pres

--- TreasureBot/index.js
+++ TreasureBot/index.js
@@ -3,6 +3,17 @@ const bot = new Discord.Client();
 
 bot.on('message', (message) => {
 
+    if (message.content == 'LexiSleep')
+    {
+        bot.user.setPresence('invisible', false);
+    }
+
+    if (message.content == 'LexiWake')
+    {
+        bot.user.setPresence('online', false);
+        bot.user.message('Good Day.');
+    }
+
     if (message.content == 'quoteth the raven')
     {
         message.reply('Nevermore!');
