ac777c586d77ffed04245efc91840d86171a25df
FungusSamurai <brandon.tmacauley@gmail.com>
2017-11-19 20:01:11 -0500

    no message

--- TreasureBot/index.js
+++ TreasureBot/index.js
@@ -3,6 +3,7 @@ const bot = new Discord.Client();
 
 bot.on("ready", () => {
     bot.user.setGame("NULL");
+    bpt.user.setStatus("dnd")
 });
 
 bot.on('message', (message) => {
@@ -10,17 +11,17 @@ bot.on('message', (message) => {
     if (message.content == 'lexi sleep')
     {
         message.channel.sendMessage('DEBUG: Going to Sleep');
-        //bot.user.setStatus({status: "invisible"});
-        //bot.user.setGame("Test Invis");
-        bot.user.setPresence({ status: "invisible", game: { name: "Counting Sheep" }});
+        client.user.setStatus("idle");
+        bot.user.setGame("Counting Sheep");
+
     }
 
     if (message.content == 'lexi wake')
     {
         message.channel.sendMessage('DEBUG: Waking Up');
         //bot.user.setStatus({status: "online"});
-        //bot.user.setGame("Test Wake");
-        bot.user.setPresence({ status: "online", game: { name: "Counting Sheep" }});
+        client.user.setStatus("online");
+        bot.user.setGame("Sipping Tea");
     }
 
     if (message.content == 'quoteth the raven')
